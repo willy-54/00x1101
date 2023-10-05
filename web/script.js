8c6a4e14395e3c85e095463c449ada8814f96498
willy-54 <71471234+willy-54@users.noreply.github.com>
2023-10-05 18:31:31 +0800

    Add files via upload

---
+++ web/script.js
@@ -0,0 +1,534 @@
+var questions = [
+    {
+        "question": "1. 研究物體之運動而不計其影響運動之因素的科學稱為",
+        "options": [
+            "A. 動力學",
+            "B. 靜力學",
+            "C. 運動學",
+            "D. 材料力學"
+        ],
+        "correctAnswer": "C"
+    },
+    {
+        "question": "2. 對力學之研究,通常可分為三部份,即剛體力學、非剛體力學及",
+        "options": [
+            "A. 靜力學",
+            "B. 動力學",
+            "C. 材料力學",
+            "D. 流體力學"
+        ],
+        "correctAnswer": "D"
+    },
+    {
+        "question": "3. 在靜力學的研討範圍內,均將受力物體假設為",
+        "options": [
+            "A. 彈性體",
+            "B. 剛體",
+            "C. 非剛體",
+            "D. 變形體"
+        ],
+        "correctAnswer": "B"
+    },
+    {
+        "question": "4. 力的觀念是",
+        "options": [
+            "A. 力可以單獨存在",
+            "B. 任何一物體都有力之表現",
+            "C. 兩物體間才會有力之表現",
+            "D. 力是一種能量"
+        ],
+        "correctAnswer": "C"
+    },
+    {
+        "question": "5. 力之傳遞可經由接觸或不接觸,下列何者為不經接觸傳遞之力?",
+        "options": [
+            "A. 桌椅對地板之壓力",
+            "B. 汽缸中蒸氣對活塞之推力",
+            "C. 摩擦力",
+            "D. 磁力"
+        ],
+        "correctAnswer": "D"
+    },
+    {
+        "question": "6. 下列何者屬於向量?",
+        "options": [
+            "A. 時間",
+            "B. 長度",
+            "C. 質量",
+            "D. 重量"
+        ],
+        "correctAnswer": "D"
+    },
+    {
+        "question": "7.質量1千克的物體,在緯度45度的海平面上所受的地心引力之大小為",
+        "options": [
+            "A. 牛頓",
+            "B. 1公斤重",
+            "C. 9.8公斤重",
+            "D. 1磅重"
+        ],
+        "correctAnswer": "B"
+    },
+    {
+        "question": "8.一質點上同時承受數個外力,若以單一力量代表該質點所承受上述外力之總和,此係利用 下列何種方法?",
+        "options": [
+            "A. 力量的分解",
+            "B. 力量的合成",
+            "C. 力偶的合成",
+            "D. 力矩的分解"
+        ],
+        "correctAnswer": "B"
+    },
+    {
+        "question": "9.當作用力之作用線與轉軸平行時,則其轉矩為",
+        "options": [
+            "A. 零",
+            "B. 無限大",
+            "C. 不一定",
+            "D. 與力臂成反比"
+        ],
+        "correctAnswer": "A"
+    },
+    {
+        "question": "10.物體轉動之難易,依其__而定",
+        "options": [
+            "A. 力的大小",
+            "B. 力矩的大小",
+            "C. 力的方向",
+            "D. 力的著力點"
+        ],
+        "correctAnswer": "B"
+    },
+    {
+        "question": "11.下列有關力偶之敘述,何者錯誤?",
+        "options": [
+            "A. 力偶是由兩個大小相等,方向相反,且不共線之二平行力所形成",
+            "B. 力偶矩之大小隨力矩軸中心位置之移動而改變",
+            "C. 力偶之合力為零",
+            "D. 力偶是向量,可適用向量之加法法則。"
+        ],
+        "correctAnswer": "B"
+    },
+    {
+        "question": "12.某一力系之合力結果為 2F=0,∑MA≠0 (A為平面上任意點),則此力系合力為何?",
+        "options": [
+            "A. 一力偶",
+            "B. 一單力",
+            "C. 力系平衡",
+            "D. 零"
+        ],
+        "correctAnswer": "A"
+    },
+    {
+        "question": "13.一物體受兩力作用而平衡時,則兩力必 1.大小相等 2.方向相反 3.力必共線,上述正確者為",
+        "options": [
+            "A. 1、2",
+            "B. 1、3",
+            "C. 2、3",
+            "D. 1、2、3"
+        ],
+        "correctAnswer": "D"
+    },
+    {
+        "question": "14.下列敘述何者錯誤?",
+        "options": [
+            "A. 任何非均質物體,其質心與形心必合而為一",
+            "B. 一個均質的球體或其球面之形心,即 其球心",
+            "C. 一個物體的重心,可視為物體全部重量均集中於該點",
+            "D. 將一物體懸吊空中,其重心必在重力作用線上"
+        ],
+        "correctAnswer": "A"
+    },
+    {
+        "question": "15.下列有關重心、形心、與質心的描述,何者不正確?",
+        "options": [
+            "A. 物體重心的合力的作用線一定會通過該物體的形心",
+            "B. 物體的質心位置可以在物體的外部",
+            "C. 物體的形心座標會因參考座標不同而改變",
+            "D. 對所有物體言,形心與質心位置會在同一點"
+        ],
+        "correctAnswer": "D"
+    },
+    {
+        "question": "16.對於剛體的敘述,下列何者錯誤?",
+        "options": [
+            "A. 剛體內各質點間之距離保持不變",
+            "B. 剛體受外力作用後,其形狀、大小皆不變",
+            "C. 力的可傳性原理可適用於剛體",
+            "D. 重型機械鋼鐵機座是剛體"
+        ],
+        "correctAnswer": "D"
+    },
+    {
+        "question": "17.下列何者為正確敘述?",
+        "options": [
+            "A. 靜力學是研究物體受力後速度改變量與時間之關係",
+            "B. 運動學是研究物體運動時,外力與質量的關係",
+            "C. 動力學是研究物體運動與導致運動因素之關係:即力學四要素之間的關係",
+            "D. 材料力學是研究力與剛體之間的關係"
+        ],
+        "correctAnswer": "C"
+    },
+    {
+        "question": "18.研究物體運動狀態之改變及其改變之原因之學問為",
+        "options": [
+            "A. 靜力學",
+            "B. 動力學",
+            "C. 彈力學",
+            "D. 運動學"
+        ],
+        "correctAnswer": "B"
+    },
+    {
+        "question": "19.下列哪一門學科,不能將物體視為剛體?",
+        "options": [
+            "A. 材料力學",
+            "B. 靜力學",
+            "C. 動力學",
+            "D. 運動學"
+        ],
+        "correctAnswer": "A"
+    },
+    {
+        "question": "20.下列何者屬於力的三要素之一?",
+        "options": [
+            "A. 質量",
+            "B. 作用點",
+            "C. 作用時間",
+            "D. 位移"
+        ],
+        "correctAnswer": "B"
+    },
+    {
+        "question": "21.下列何者屬於內效應?",
+        "options": [
+            "A. 旋轉",
+            "B. 位移",
+            "C. 運動",
+            "D. 變形"
+        ],
+        "correctAnswer": "D"
+    },
+    {
+        "question": "22.下列何者為力對物體之外效應?",
+        "options": [
+            "A. 支承反力",
+            "B. 剪力",
+            "C. 應力",
+            "D. 變形"
+        ],
+        "correctAnswer": "A"
+    },
+    {
+        "question": "23.下列之敘述,何者有誤?",
+        "options": [
+            "A. 力的三要素為大小,方向及作用點",
+            "B. 力偶矩是屬於自由向量",
+            "C. 純量是指沒有單位的物理量",
+            "D. 研究物體之運動,常視物體為一質點"
+        ],
+        "correctAnswer": "C"
+    },
+    {
+        "question": "24.下列敘述中何者為錯誤?",
+        "options": [
+            "A. 重量是向量,質量是純量",
+            "B. 物體受外力作用時,體內各質點間之距離不會改變,此物體謂之剛體",
+            "C. 力的三要素是大小、方向、作用時間",
+            "D. 向量可分類為自由向量、滑動向量、固定向量等三種"
+        ],
+        "correctAnswer": "C"
+    },
+    {
+        "question": "25.研究力對物體所產生的內效應時,必須把力當作何種向量處理?",
+        "options": [
+            "A. 滑動向量",
+            "B. 自由向量",
+            "C. 固定向量",
+            "D. 對稱向量"
+        ],
+        "correctAnswer": "C"
+    },
+    {
+        "question": "26.分析靜力學、動力學之作用力可視為何種向量?",
+        "options": [
+            "A. 自由向量",
+            "B. 滑動向量",
+            "C. 固定向量",
+            "D. 以上皆是"
+        ],
+        "correctAnswer": "B"
+    },
+    {
+        "question": "27.使物體產生變形效應之力為",
+        "options": [
+            "A. 自由向量",
+            "B. 滑動向量",
+            "C. 固定向量",
+            "D. 純量"
+        ],
+        "correctAnswer": "C"
+    },
+    {
+        "question": "28.下列何者非自由向量?",
+        "options": [
+            "A. 角加速度",
+            "B. 角速度",
+            "C. 力矩",
+            "D. 力偶"
+        ],
+        "correctAnswer": "C"
+    },
+    {
+        "question": "29.在 CGS 制中,下列何者為力的國際單位?",
+        "options": [
+            "A. kg-m/sec",
+            "B. kg-m/sec²",
+            "C. g-cm/sec",
+            "D. g-cm/sec²"
+        ],
+        "correctAnswer": "D"
+    },
+    {
+        "question": "30.在MKS 制中,力的國際單位是N(牛頓)x",
+        "options": [
+            "A. kg-m/s",
+            "B. g-cm/s",
+            "C. kg-m-s",
+            "D. kg-m/s²"
+        ],
+        "correctAnswer": "D"
+    },
+    {
+        "question": "31.使質量1仟克的物體,產生1米/秒2之加速度時,所需的力稱為",
+        "options": [
+            "A. 1 達因",
+            "B. 1 耳格",
+            "C. 1 牛頓",
+            "D. 1 焦耳"
+        ],
+        "correctAnswer": "C"
+    },
+    {
+        "question": "32.作用於物體的外力,可沿作用線方向改變其作用點,所產生外效應不變,此為力之原理。",
+        "options": [
+            "A. 內效應",
+            "B. 可傳性",
+            "C. 撓性",
+            "D. 彈性"
+        ],
+        "correctAnswer": "B"
+    },
+    {
+        "question": "33.質點通常被視為",
+        "options": [
+            "A. 無體積而具有質量之物體",
+            "B. 無體積無質量之物體",
+            "C. 無面積而具有質量之物體",
+            "D. 無面積無質量之物體"
+        ],
+        "correctAnswer": "A"
+    },
+    {
+        "question": "34.同平面上兩作用力同時作用於一點,若其合力為最大時,則該二力之夾角為",
+        "options": [
+            "A. 0°",
+            "B. 90°",
+            "C. 120°",
+            "D. 180°"
+        ],
+        "correctAnswer": "A"
+    },
+    {
+        "question": "35.下列有關力偶的敘述,何者錯誤?",
+        "options": [
+            "A. 力偶為作用於一物體之兩力,其大小相等,方向相反,且不在同一直線上之兩平行力",
+            "B. 力偶三要素為力偶矩之大小、力偶旋轉之方向及力偶作用面之方位",
+            "C. 力偶矩之單位與力矩之單位相同",
+            "D. 力偶為一純量"
+        ],
+        "correctAnswer": "D"
+    },
+    {
+        "question": "36. 下列有關力矩之敘述,何者錯誤?",
+        "options": [
+            "A. 與力矩軸平行的力量對此軸之力矩為零",
+            "B. 與力矩軸相交的力量對此軸之力矩為零",
+            "C. 力沿著作用線移動時,會改變力矩之大小",
+            "D. 物體所受力矩愈大,表示此物體轉動的趨勢愈大"
+        ],
+        "correctAnswer": "C"
+    },
+    {
+        "question": "37. 下列有關力偶轉換性的敘述,何者錯誤?",
+        "options": [
+            "A. 力偶可在其作用平面上任意移動或轉動",
+            "B. 若力偶的大小與方向不改變時,力偶的二平行力與其力偶臂可任意改變",
+            "C. 力偶可任意移至與原作用平行之平面上",
+            "D. 力偶的作用面可任意改變"
+        ],
+        "correctAnswer": "D"
+    },
+    {
+        "question": "38.同平面一般力系以力的多邊形與索線多邊形法求解時,當力的多邊形封閉,而索線多邊形 中,首索和尾索互相平行,則合力為",
+        "options": [
+            "A. 一單力",
+            "B. 一力偶",
+            "C. 零",
+            "D. 一力矩"
+        ],
+        "correctAnswer": "B"
+    },
+    {
+        "question": "39.下列何者為力偶合成原理之敘述?",
+        "options": [
+            "A. 若力系之合力及合力偶皆等於零,則力系於點 A 之外效應等於零",
+            "B. 若一物體受力系之合力及合力偶等於零,則此物體之外效應為零",
+            "C. 若一物體受力系之合力及合力偶等於零,則此物體之內效應亦等於零",
+            "D. 若一物體受力系之合力等於零,則此物體之合力偶必等於零"
+        ],
+        "correctAnswer": "A"
+    },
+    {
+        "question": "40. 質點自由體圖之特性,下列何者錯誤?",
+        "options": [
+            "A. 質點自由體圖即是將此質點之受力情形表示於紙上",
+            "B. 質點自由體圖之畫法是以質點為中心,將受力之向量畫出",
+            "C. 質點自由體圖之力向量可在紙上將其畫線平移而不變之",
+            "D. 質點自由體圖之力向量必合力及合力偶同時畫出"
+        ],
+        "correctAnswer": "D"
+    }
+];
+
+var currentQuestionIndex = 0;
+var currentQuizQuestion = 0;
+var userAnswers = [];
+
+function showQuestions() {
+    document.getElementById("questionContainer").style.display = "block";
+    document.getElementById("quizContainer").style.display = "none";
+    displayAllQuestions();
+}
+
+function displayAllQuestions() {
+    var questionContainer = document.getElementById("questionContainer");
+    questionContainer.innerHTML = "";
+
+    for (var i = 0; i < questions.length; i++) {
+        var questionObj = questions[i];
+        
+        var questionDiv = document.createElement("div");
+        
+        var questionContent = document.createElement("ul");
+        var questionText = questionObj.question;
+
+        for (var j = 0; j < questionObj.options.length; j++) {
+            var option = questionObj.options[j];
+            if (option.charAt(0) === questionObj.correctAnswer) {
+                option = '<span style="color:red;">' + option + '</span>';
+            }
+            questionText += "<br>" + option;
+        }
+
+        questionContent.innerHTML = questionText;
+
+        questionDiv.appendChild(questionContent);
+        
+        questionContainer.appendChild(questionDiv);
+        
+        if (i < questions.length - 1) {
+            questionContainer.appendChild(document.createElement("br"));
+        }
+    }
+}
+
+function startQuiz() {
+    document.getElementById("questionContainer").style.display = "none";
+    document.getElementById("quizContainer").style.display = "block";
+    displayQuizQuestion();
+}
+
+function previousQuestion() {
+    if (currentQuizQuestion > 0) {
+        currentQuizQuestion--;
+        displayQuizQuestion();
+    }
+}
+
+function nextQuestion() {
+    if (currentQuizQuestion < questions.length - 1) {
+        currentQuizQuestion++;
+        displayQuizQuestion();
+    }
+}
+
+function displayQuizQuestion() {
+    if (currentQuizQuestion < questions.length) {
+        var questionObj = questions[currentQuizQuestion];
+        document.getElementById("quizQuestion").textContent = questionObj.question;
+        
+        var optionsList = document.getElementById("quizOptionsList");
+        optionsList.innerHTML = "";
+        
+        for (var i = 0; i < questionObj.options.length; i++) {
+            var option = questionObj.options[i];
+            var listItem = document.createElement("li");
+            var radioButton = '<input type="radio" name="q1" value="' + option.charAt(0) + '"> ';
+            listItem.innerHTML = radioButton + option;
+    
+            listItem.querySelector("input").addEventListener("change", function () {
+                userAnswers[currentQuizQuestion] = this.value;
+            });
+    
+            optionsList.appendChild(listItem);
+        }
+    } else {
+        document.getElementById("quizResult").textContent = "測驗完成";
+        document.getElementById("quizSubmitButton").style.display = "none";
+    }
+
+    document.getElementById("previousButton").disabled = currentQuizQuestion === 0;
+    document.getElementById("nextButton").disabled = currentQuizQuestion === questions.length - 1;
+}
+
+function calculateScore() {
+    var resultContainer = document.getElementById("quizResult");
+    resultContainer.innerHTML = "<h2>結果：</h2><ul>";
+
+    for (var i = 0; i < questions.length; i++) {
+        var questionObj = questions[i];
+        var userAnswer = userAnswers[i];
+        var isCorrect = userAnswer === questionObj.correctAnswer;
+
+        var listItem = "<li>" + questionObj.question + " - <span class='" + (isCorrect ? "correct" : "incorrect") + "'>" + (isCorrect ? "正確" : "錯誤") + "</span>";
+
+        if (!isCorrect) {
+            if (questionObj.correctAnswer === 'A') {
+                listItem += `<span style="color:blue;"> (正確答案:  ${questionObj.options[0]})`
+            } else if (questionObj.correctAnswer === 'B') {
+                listItem += `<span style="color:blue;"> (正確答案:  ${questionObj.options[1]})`
+            } else if (questionObj.correctAnswer === 'C') {
+                listItem += `<span style="color:blue;"> (正確答案:  ${questionObj.options[2]})`
+            } else if (questionObj.correctAnswer === 'D') {
+                listItem += `<span style="color:blue;"> (正確答案:  ${questionObj.options[3]})`
+            }
+        }
+
+        listItem += "</li>";
+
+        resultContainer.innerHTML += listItem;
+
+    }
+
+    resultContainer.innerHTML += "</ul>";
+}
+
+
+function resetQuiz() {
+    currentQuizQuestion = 0;
+    userAnswers = [];
+    document.getElementById("quizResult").textContent = "";
+    document.getElementById("quizSubmitButton").style.display = "block";
+    displayQuizQuestion();
+}
